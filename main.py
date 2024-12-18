68a3a1cce4bbf8432cb8d10cd8f3f428f635ce7d
wjm <3201884732@qq.com>
2024-12-18 20:01:13 +0800

    '更新html文件，更新main.py，更新requirements.txt'

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 import signal
 import os
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtWebEngineCore import QWebEngineSettings
-from PyQt6.QtCore import QUrl
+from PyQt6.QtWebEngineCore import QWebEngineSettings, QWebEngineProfile
+from PyQt6.QtCore import QUrl, Qt
 from PyQt6.QtWidgets import QApplication
 from backend import start_server
 import threading
@@ -20,10 +20,20 @@ relative_html_path = 'gaodemap.html'
 full_html_path = os.path.join(current_script_path, relative_html_path)
 qurl = QUrl.fromLocalFile(full_html_path)
 
+
+profile = QWebEngineProfile.defaultProfile()
+
+profile.setHttpCacheType(QWebEngineProfile.HttpCacheType.MemoryHttpCache)
+profile.setHttpCacheMaximumSize(100000000) # 10MB
+app.setAttribute(Qt.ApplicationAttribute.AA_UseOpenGLES,True)
 browser = QWebEngineView()
 browser.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls, True)
 browser.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessFileUrls, True)
 browser.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptEnabled, True)
+browser.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanOpenWindows, False)
+browser.settings().setAttribute(QWebEngineSettings.WebAttribute.AutoLoadImages, True)
+browser.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalStorageEnabled, True)
+
 browser.setUrl(qurl)
 browser.show()
 
